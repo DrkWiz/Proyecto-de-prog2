3a8e7141db6567a298616551eab36a78dea0cc25
Nicolas <nicolasantiagomez4@gmail.com>
2023-06-01 15:01:51 -0300

    Changes by francisco

--- CLION/librerias/tablero.cpp
+++ CLION/librerias/tablero.cpp
@@ -1,1 +1,44 @@
 #include "tablero.h"
+tablero::tablero(short _l) {
+    setL(l);
+}
+
+void tablero::setL(short _l) {
+    l = _l;
+}
+
+void tablero::setTablero() {
+
+
+
+}
+
+void tablero::llenarVector() {
+
+}
+
+void tablero::mostrar() {
+
+    for(short i = 0; i < l; i++)
+    {
+        for(short j = 0; j < l; j++)
+        {
+            cout<<"( "<<casillas[i][j].getX()<<", "<<casillas[i][j].getY()<<")";
+        }
+    }
+
+}
+
+casilla tablero::getCasilla(short pos1,short pos2) {
+int i=pos1,j=pos2;
+    return casillas[i][j];
+}
+
+tablero::~tablero() {
+    for(casilla *p : casillas)
+    {
+        delete p;
+    }
+
+    casillas.clear();
+}
